1538fcdbdb07ffcaf899514d4630fc3744edb6c3
Nelson Ruddy Paulino De Jesus <nelsonruddy@gmail,com>
2024-10-25 15:44:50 -0400

    loading all code

--- ApiBook/BookServiceApi/obj/Release/net8.0/BookServiceApi.AssemblyInfo.cs
+++ ApiBook/BookServiceApi/obj/Release/net8.0/BookServiceApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookServiceApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d65e462208f46e1a2d7f84305dd7fb32a2edadb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookServiceApi")]
